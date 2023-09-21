5b86f2439f4ce3d332a237492a70e0a9c07a2336
adhithiyan <adhithiyan.work@gmail.com>
2023-09-21 15:46:16 +0530

    add: all necessary files and packages for a development environment(package.json, webpack and relative plugins)

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("My name Quent")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDRCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiTXkgbmFtZSBRdWVudFwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
